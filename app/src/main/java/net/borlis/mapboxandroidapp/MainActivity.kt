2451007b7b59728f35eeac8c2c4fe157e7e00c69
Olga Borlis <o.borlis@itransition.com>
2021-01-29 14:08:32 +0200

    Initial commit

---
+++ app/src/main/java/net/borlis/mapboxandroidapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.borlis.mapboxandroidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
